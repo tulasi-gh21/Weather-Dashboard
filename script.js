1d245e1bff9031c6c4d048554f0aff9c0dcb8507
Tulasi-Gowdru-Halesh <tulasigowdruhalesh21@gmail.com>
2026-05-08 07:01:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let apiKey = "ff6f0874ccad6869f1963d66a1762e3a";
+
 async function getWeather() {
+
     let city = document.getElementById("cityInput").value;
 
     if (city === "") {
@@ -6,34 +9,147 @@ async function getWeather() {
         return;
     }
 
-    let apiKey = "ff6f0874ccad6869f1963d66a1762e3a";
+    getWeatherData(city);
+}
 
-    let url =
+async function getWeatherData(city) {
+
+    let weatherUrl =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
+        encodeURIComponent(city) +
+        "&appid=" +
+        apiKey +
+        "&units=metric";
+
+    let forecastUrl =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        encodeURIComponent(city) +
         "&appid=" +
         apiKey +
         "&units=metric";
 
     try {
-        let response = await fetch(url);
-        let data = await response.json();
 
-        if (response.ok === false) {
+        let weatherResponse = await fetch(weatherUrl);
+
+        let weatherData = await weatherResponse.json();
+
+        if (weatherResponse.ok === false) {
+
             document.getElementById("weatherResult").innerHTML =
-                "Error: " + data.message;
+                "Error: " + weatherData.message;
+
             return;
         }
 
+        let iconCode = weatherData.weather[0].icon;
+
+        let iconUrl =
+            "https://openweathermap.org/img/wn/" +
+            iconCode +
+            "@2x.png";
+
         document.getElementById("weatherResult").innerHTML =
-            "<h2>" + data.name + "</h2>" +
-            "<p>Temperature: " + data.main.temp + " °C</p>" +
-            "<p>Humidity: " + data.main.humidity + "%</p>" +
-            "<p>Condition: " + data.weather[0].description + "</p>" +
-            "<p>Wind Speed: " + data.wind.speed + " m/s</p>";
+
+            '<div class="weather-card">' +
+
+            "<h2>" + weatherData.name + "</h2>" +
+
+            '<img src="' + iconUrl + '" alt="Weather Icon">' +
+
+            "<p>Temperature: " +
+            weatherData.main.temp +
+            " °C</p>" +
+
+            "<p>Humidity: " +
+            weatherData.main.humidity +
+            "%</p>" +
+
+            "<p>Condition: " +
+            weatherData.weather[0].description +
+            "</p>" +
+
+            "<p>Wind Speed: " +
+            weatherData.wind.speed +
+            " m/s</p>" +
+
+            "</div>";
+
+        let forecastResponse = await fetch(forecastUrl);
+
+        let forecastData = await forecastResponse.json();
+
+        let forecastHTML =
+            '<div class="forecast-container">';
+
+        for (let i = 0; i < 5; i++) {
+
+            let item = forecastData.list[i * 8];
+
+            let forecastIcon =
+                "https://openweathermap.org/img/wn/" +
+                item.weather[0].icon +
+                "@2x.png";
+
+            forecastHTML +=
+
+                '<div class="forecast-card">' +
+
+                "<h4>" +
+                item.dt_txt.split(" ")[0] +
+                "</h4>" +
+
+                '<img src="' +
+                forecastIcon +
+                '" alt="Forecast Icon">' +
+
+                "<p>" +
+                item.main.temp +
+                " °C</p>" +
+
+                "<p>" +
+                item.weather[0].description +
+                "</p>" +
+
+                "</div>";
+        }
+
+        forecastHTML += "</div>";
+
+        document.getElementById("forecast").innerHTML =
+            forecastHTML;
 
     } catch (error) {
+
         document.getElementById("weatherResult").innerHTML =
-            "Network error. Please check your internet connection.";
+            "Error loading weather data.";
     }
+}
+
+function getCurrentLocationWeather() {
+
+    navigator.geolocation.getCurrentPosition(
+
+        async function (position) {
+
+            let lat = position.coords.latitude;
+
+            let lon = position.coords.longitude;
+
+            let url =
+                "https://api.openweathermap.org/data/2.5/weather?lat=" +
+                lat +
+                "&lon=" +
+                lon +
+                "&appid=" +
+                apiKey +
+                "&units=metric";
+
+            let response = await fetch(url);
+
+            let data = await response.json();
+
+            getWeatherData(data.name);
+        }
+    );
 }
